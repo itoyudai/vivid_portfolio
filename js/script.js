3aa8c4a332d4a446072f0a057e9b364b5ce2b408
itoyudai <32475461+itoyudai@users.noreply.github.com>
2017-10-18 13:16:24 +0900

    スムーズスクロールなどを追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(function(){
-
+	$('a[href^=#]').click(function(){
+		var speed = 1000;
+		var href = $(this).attr('href');
+		var target = $(href === '#' || href === '' ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position}, speed, 'swing');
+		return false;
+	});
+	$('#totop a').hover(function(){
+		$(' img',this).attr('src','image/totop_after.png');
+	},function(){
+		$(' img',this).attr('src','image/totop_before.png');
+	})
 })
