fd081e64dcc0c294fef6869ae405ca1824db1162
itoyudai <32475461+itoyudai@users.noreply.github.com>
2017-10-19 12:32:35 +0900

    記述の修正

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 $(function(){
-	$('a[href^=#]').click(function(){
-		var speed = 1000;
+
+	var totop_image = [
+		totopAfter = 'image/totop_after.png',
+		totopBefore = 'image/totop_before.png'
+	];
+
+	$('a[href ^= "#"]').click(function(){
+		var SCROLL_SPEED = 1000;
 		var href = $(this).attr('href');
-		var target = $(href === '#' || href === '' ? 'html' : href);
+		var target = $(href == '#' || href == '' ? 'html' : href);
 		var position = target.offset().top;
-		$('body,html').animate({scrollTop:position}, speed, 'swing');
+		$('body,html').animate({scrollTop:position}, SCROLL_SPEED, 'swing');
 		return false;
 	});
 	$('#totop a').hover(function(){
-		$(' img',this).attr('src','image/totop_after.png');
+		$('img',this).attr('src',totopAfter);
 	},function(){
-		$(' img',this).attr('src','image/totop_before.png');
-	})
-})
+		$('img',this).attr('src',totopBefore);
+	});
+
+});
